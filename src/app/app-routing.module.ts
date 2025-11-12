bf417bc638a006bc43fc1752b728e7eef6936e0a
ClementCar <clementcarcenac@gmail.com>
2025-11-12 17:23:12 +0100

    bg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { DriverInfoComponent } from './components/driver-info/driver-info.component';
+import { TeamInfoComponent } from './components/team-info/team-info.component';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule) },
+  { path: 'team-info', component: TeamInfoComponent },
   { path: 'driver-info', component: DriverInfoComponent },
   { path: '**', redirectTo: ''}
 ];

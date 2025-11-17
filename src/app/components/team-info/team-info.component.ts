927103e9e5e1c421be2e9b9e792b31d3f44c28bd
ClementCar <clementcarcenac@gmail.com>
2025-11-17 15:54:19 +0100

    in

--- src/app/components/team-info/team-info.component.ts
+++ src/app/components/team-info/team-info.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Team } from 'src/app/config/teams';
 import { TeamsService } from 'src/app/services/teams-service';
+import { UtilityService } from 'src/app/services/utility';
 
 @Component({
   selector: 'app-team-info',
@@ -14,11 +15,19 @@ export class TeamInfoComponent  implements OnInit {
   name!: string;
   team!: Team;
 
-  constructor(private route: ActivatedRoute, private teamService: TeamsService) { }
+  constructor(private route: ActivatedRoute, private teamService: TeamsService, private utilityService: UtilityService) { }
 
   ngOnInit() {
     this.name = this.route.snapshot.paramMap.get('name')!;
     this.team = this.teamService.getTeam(this.name)!;
+  }
+
+  lightenDarkenColor(col: string, amt: number): string {
+    return this.utilityService.getlightenDarkenColor(col, amt);
+  }
+
+  getText(text: string){
+    return this.utilityService.getLowerText(text);
   }
 
 }

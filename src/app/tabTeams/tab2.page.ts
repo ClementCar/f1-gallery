3a128d846faedff205707332ef7c799f2d85f188
ClementCar <clementcarcenac@gmail.com>
2025-11-21 17:14:55 +0100

    entry anim

--- src/app/tabTeams/tab2.page.ts
+++ src/app/tabTeams/tab2.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { Team, Teams } from '../config/teams';
 import { UtilityService } from '../services/utility';
 import { Router } from '@angular/router';
@@ -9,11 +9,38 @@ import { Router } from '@angular/router';
   styleUrls: ['tab2.page.scss'],
   standalone: false,
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit, OnDestroy, AfterViewInit {
 
   teams: Team[] = Teams;
+  neonFinished: Record<string, boolean> = {};
+  seen: Record<string, boolean> = {};
+  observer!: IntersectionObserver;
 
   constructor(private utilityService: UtilityService, private route: Router) {}
+
+  ngOnInit(): void {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          const target = entry.target as HTMLElement;
+          const teamName = target.getAttribute('data-team');
+          if(!teamName) return;
+
+          if(entry.isIntersecting && !this.seen[teamName]){
+            this.seen[teamName] = true;
+            target.classList.add('start');
+          }
+        })
+      },
+      { threshold: 0.8}
+    )
+  }
+
+  ngAfterViewInit(): void {
+    document.querySelectorAll('.neon-anim').forEach( el => {
+      this.observer.observe(el);
+    })
+  }
 
   getText(text: string){
     return this.utilityService.getLowerText(text);
@@ -25,6 +52,18 @@ export class Tab2Page {
 
   selectTeam(team: Team) {
     this.route.navigate(['team-info', team.name])
+  }
+
+  onNeonDone(name: string){
+    this.neonFinished[name] = true;
+  }
+
+  isNeonDone(name: string): boolean{
+    return !!this.neonFinished[name];
+  }
+
+  ngOnDestroy(): void {
+    this.observer.disconnect();
   }
 
 }

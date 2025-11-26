7cdf61e6f9d59b51a5c7276b5971daf34d16f47e
ClementCar <clementcarcenac@gmail.com>
2025-11-26 16:52:50 +0100

    driver anim

--- src/app/components/driver-card/driver-card.component.ts
+++ src/app/components/driver-card/driver-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { Driver, Team } from 'src/app/config/teams';
 import { UtilityService } from 'src/app/services/utility';
 
@@ -8,16 +8,36 @@ import { UtilityService } from 'src/app/services/utility';
   styleUrls: ['./driver-card.component.scss'],
   standalone: false
 })
-export class DriverCardComponent  implements OnInit {
+export class DriverCardComponent  implements OnInit, OnDestroy, AfterViewInit {
 
   @Input() team!: Team;
   @Input() driver!: Driver;
 
   @Output() select = new EventEmitter();
 
+  neonFinished: Record<string, boolean> = {};
+  seen: Record<string, boolean> = {};
+  observer!: IntersectionObserver;
+
   constructor(private utilityService: UtilityService) { }
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          const target = entry.target as HTMLElement;
+          const driverName = target.getAttribute('data-driver');
+          if(!driverName) return;
+
+          if(entry.isIntersecting && !this.seen[driverName]){
+            this.seen[driverName] = true;
+            target.classList.add('start');
+          }
+        })
+      },
+      { threshold: 0.8 }
+    )
+  }
 
   selectDriver() {
     this.select.emit();
@@ -31,4 +51,21 @@ export class DriverCardComponent  implements OnInit {
     return this.utilityService.getlightenDarkenColor(col, amt);
   }
 
+  ngAfterViewInit(): void {
+    document.querySelectorAll('.neon-anim').forEach(el => {
+      this.observer.observe(el);
+    })
+  }
+
+  onNeonDone(driver: string){
+    this.neonFinished[driver] = true;
+  }
+
+  isNeonDone(driver: string){
+    return !!this.neonFinished[driver];
+  }
+
+  ngOnDestroy(): void {
+    this.observer.disconnect();
+  }
 }

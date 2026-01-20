cd569f57ff63fd86f708dcea0a8eb5d696771d91
ClementCar <clementcarcenac@gmail.com>
2026-01-20 16:34:02 +0100

    service / card

---
+++ src/app/services/open-api-service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OpenApiService {
+  
+  // https://api.openf1.org/v1/meetings?year=2026&country_name=Singapore
+
+  // meatiing, Sessions, sting, location
+
+}

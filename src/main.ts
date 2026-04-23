6dd085e2fdb189b2e4dbfb228b86ca5f1ca1715a
ClementCar <clementcarcenac@gmail.com>
2026-04-23 17:01:47 +0200

    Team page rework

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
+import { inject } from "@vercel/analytics";
+
 import { AppModule } from './app/app.module';
+
+inject();
 
 defineCustomElements(window);
 
